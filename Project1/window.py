d3387cc968a56f9b0fe22e34098ab23347f1be05
Kyle Thai <63701122+kyoutay@users.noreply.github.com>
2022-06-15 12:55:29 -0700

    Update window.py

--- Project1/window.py
+++ Project1/window.py
@@ -1,8 +1,8 @@
-import pygame as pg
+import pygame as pg #so we can use "pg." instead of typing out "pygame." everytime 
 
 pg.init()
 
-background_color = pg.Color('green')
+background_color = pg.Color('green') #in rgb would be background_color = (234, 212, 252)
 
 screen = pg.display.set_mode((500,500)) #window is 500 by 500
 pg.display.set_caption('WindowGoBrr')
@@ -20,6 +20,6 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    pg.display.flip()
+    pg.display.flip() #update screen all the time
 
 pg.quit()
