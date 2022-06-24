6d7999f01f53f04a627124eede2b66afc730b654
Kyle Thai <63701122+kyoutay@users.noreply.github.com>
2022-06-23 20:12:10 -0700

    Update part1_cookieClicker.py

--- Project3/part1_cookieClicker.py
+++ Project3/part1_cookieClicker.py
@@ -1,66 +1,128 @@
-#credits to PSpeiser for a lot of the code https://github.com/PSpeiser/cookieclicker 
-
+#credits to: https://github.com/PSpeiser/cookieclicker for the cookie logic and shop class
 
 import os
 import sys
 import pygame as pg
-from pygame.locals import *
-import random
+import random #for the random ant spawning we'll add later
 
-class shop:
+class shop: #only ant and shop are classes since we need multiple ants and multiple shop items. Classes serve as cookie cutters or blueprints to make a lot of these same objects
     def __init__(self, rect, text, base_price, base_cps_each):
         self.rect = rect
         self.text = text
-        self.count = 0
+        self.level = 0
         self.base_price = base_price
         self.cps_each = base_cps_each
-        self.hidden = True
-    
-    def shopIcon(self, surface):
-        surface.blit(shopImg,(800,800))
 
     def draw(self, surface):
-        pg.draw.rect(surface, pg.Color('gray'), self.rect,0) # button background color
-        text_surface = font.render('Level: ' + str(self.count) + ' ' + self.text + ' $' + str(int(self.price())), False, pg.Color('black'))
+        pg.draw.rect(surface, pg.Color('gray'), self.rect ,0) # button background color
+        text_surface = font.render('Level: ' + str(self.level) + ' ' + self.text + ' $' + str(int(self.price())), False, pg.Color('black'))
         text_rect = text_surface.get_rect()
         text_rect.topleft = (self.rect.left +10, self.rect.top + self.rect.height * 0.25)
         surface.blit(text_surface,text_rect)
+            
 
     def total_cps(self):
-        return self.cps_each * self.count
+        return self.cps_each * self.level
 
     def price(self):
-        return self.base_price * 1.15**self.count
+        return self.base_price * 1.15**self.level
 
     def click(self):
         price = self.price()
         global COOKIES
         if COOKIES >= price:
-            self.count += 1
+            lvlUpSnd.play()
+            self.level += 1
             COOKIES -= price
     
     def collidepoint(self,point):
         return self.rect.collidepoint(point)
 
-class Ants(pg.sprite.Sprite):
+    
+class Ant(pg.sprite.Sprite):
     def __init__(self):
-        pg.sprite.Sprite.__init__(self)
-        #self.image = antImg
+        pg.sprite.Sprite.__init__(self) #calling sprite initializer 
+        self.image = antImg
+        self.originalimage = self.image
+        self.rect = self.image.get_rect()
+        self.spawn()
 
+    def spawn(self):
+        self.direction = random.randint(1,4)
+        if self.direction == 1: #downwards
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.Xspeed = 0
+            self.Yspeed = random.randrange(1, 8)
+            self.image = pg.transform.rotate(self.originalimage, 180)
+        elif self.direction == 2: #upwards
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(HEIGHT, HEIGHT+60)
+            self.Xspeed = 0
+            self.Yspeed = -random.randrange(1, 8)
+            self.image = self.originalimage
+        elif self.direction == 3: #right
+            self.rect.x = random.randrange(-100, -40)
+            self.rect.y = random.randrange(HEIGHT - self.rect.height)
+            self.Xspeed = random.randrange(1, 8)
+            self.Yspeed = 0
+            self.image = pg.transform.rotate(self.originalimage, -90) #If I want my ant to face right, I can do -90 or 270 degrees
+        elif self.direction == 4: #left
+            self.rect.x = random.randrange(WIDTH, WIDTH+60)
+            self.rect.y = random.randrange(HEIGHT - self.rect.height)
+            self.Xspeed = -random.randrange(1, 8)
+            self.Yspeed = 0
+            self.image = pg.transform.rotate(self.originalimage, 90)
 
+    def mouseCollision(self,mouse):
+        if self.rect.collidepoint(mouse):
+            squishSnd.play()
+            self.kill() #part of the sprite class
+
+    def cookieCollision(self):
+        global HEALTHPOINTS
+        if circleRectCollision(500,500,280,self.rect.left,self.rect.top,self.rect.width,self.rect.height):
+            crunchSnd.play()
+            self.kill()
+            HEALTHPOINTS -= 5
+
+    def update(self):
+        self.rect.x += self.Xspeed #keep the ant's rect up with the ant 
+        self.rect.y += self.Yspeed
+
+        if self.direction == 1: #if the ant is facing downwards and crosses the border by 10 pixels, then spawn the dude again using self.spawn()
+            if self.rect.top > HEIGHT + 10:
+                self.spawn()
+        elif self.direction == 2: #same but upwards
+            if self.rect.bottom < -10:
+                self.spawn()
+        elif self.direction == 3: # right
+            if self.rect.left > WIDTH + 10:
+                self.spawn()
+        elif self.direction == 4: #left
+            if self.rect.right < -10:
+                self.spawn()
 
 pg.init()
 
-screen = pg.display.set_mode((1000,1000)) #becomes a surface object
+WIDTH = 1000 #all caps is a global variable
+HEIGHT = 1000
+COOKIES = 0 
+HEALTHPOINTS = 100
+
+screen = pg.display.set_mode((WIDTH,HEIGHT)) #becomes a surface object
 pg.display.set_caption('Project3')
 
-COOKIES = 0
-CPS = 0.0
-FPS = 30
-fpsClock = pg.time.Clock()
-antSpeed = 10
-font = pg.font.Font(None, 24)
+def circleRectCollision(cx,cy,cr,rx,ry,rw,rh): #circle's x,y,radius and rectangle's x,y,width,height
+    circleDistX = abs(cx-rx)
+    circleDistY = abs(cy-ry)
 
+    if circleDistX > (rw/2 + cr) or circleDistY > (rh/2 + cr): return False
+    if circleDistX <= rw/2 or circleDistY <= rh/2: return True
+
+    cornerDist_sq = (circleDistX - rw/2)**2 + (circleDistY - rh/2)**2
+
+    return cornerDist_sq <= (cr**2)
 
 def load_img(name):
     filePath = os.path.join(sys.path[0], name)
@@ -72,89 +134,31 @@ def load_sound(name):
     sound = pg.mixer.Sound(filePath)
     return sound
 
-def make_items(text_list, base_price_list, cps_list, rect, spacing):
-    button_height = rect.height / len(text_list)
-    button_width = rect.width
-    buttons = []
-    for i in range(len(text_list)):
-        text = text_list[i]
-        base_price = base_price_list[i]
-        base_cps = cps_list[i]
-        button_rect = Rect(rect.left, rect.top + i * (button_height + spacing), button_width, button_height)
-        button = shop(button_rect, text, base_price, base_cps)
-        buttons.append(button)
-    return buttons
 
-def click_cookie():
-    global COOKIES
-    COOKIES += 1
-
-def calculate_cps():
-    global CPS
-    cps = 0.0
-    for item in items:
-        cps += item.total_cps()
-    CPS = cps
-
-def update_cookies():
-    global COOKIES
-    COOKIES += CPS / FPS
-
-items = make_items(["Cursor", "Grandma", "Farm", "Factory", "Mine", "Shipment", "Alchemy Lab", "Portal",
-                    "Time machine", "Antimatter condenser", "Prism"],
-                   [15, 100, 500, 3000, 10000, 40000, 200000, 1666666, 123456789, 3999999999, 75000000000],
-                   [0.1, 0.5, 4, 10, 40, 100, 400, 6666, 98765, 999999, 10000000],
-                   Rect(400, 25, 230, 400), 5)
 
 cookieImg = load_img('cookie.png')
-#antImg = load_img('ant.png')
+antImg = pg.transform.scale(load_img('ant.png'), (40,40)) #the actual image is too large, so we scale it down to 40x40
 bckgrImg = load_img('floor.jpg')
 shopImg = load_img('shop.png')
-#squishSnd = load_sound('bugSquish.mp3')
-tapSnd = load_sound('tap.wav')
 
-cookie_rect = Rect(25, 250, cookieImg.get_width(), cookieImg.get_height())
-
-
-
+squishSnd = load_sound('squish.wav')
+tapSnd = load_sound('cookieTap.wav')
+shopSnd = load_sound('shop.wav')
+lvlUpSnd = load_sound('levelUp.wav')
+crunchSnd = load_sound('crunch.wav')
+startSnd = load_sound('COOKIE.wav')
 
 
 running = True
 while running:
-
-    screen.blit(bckgrImg,(0,0))
-    screen.blit(cookieImg, cookie_rect)
-
-    #draw cookies count
-    text_surface = font.render(str(int(COOKIES)) + "+" + str(CPS) + "CPS", False, pg.Color('white'))
-    text_rect = text_surface.get_rect()
-    text_rect.topleft = (100, 200)
-    screen.blit(text_surface, text_rect)
-
-    for button in items:
-        button.draw(screen)
-
-    calculate_cps()
-    update_cookies()
 
     for event in pg.event.get():
 
         if event.type == pg.QUIT:
             running = False
 
-        if event.type == MOUSEBUTTONDOWN:
-            mouse_pos = event.pos
-            mouse_button = event.button
-            if mouse_button == 1:
-                for button in items:
-                    if button.collidepoint(mouse_pos):
-                        button.click()
-                        break
-                if cookie_rect.collidepoint(mouse_pos):
-                    click_cookie()
-
+        if event.type == pg.MOUSEBUTTONDOWN:
 
     pg.display.update()
-    fpsClock.tick(FPS) 
 
 pg.quit()
